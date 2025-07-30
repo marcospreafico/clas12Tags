c6db503a0175174cd0ae4ab0277e1e5de5899451
Marco Spreafico <sprea@SpreaBook-Pro.local>
2025-07-30 11:40:48 +0200

    Added BDX geometry (modular layout with CsI, proto geometry)

---
+++ experiments/bdx/build_detector.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl -w
+
+use strict;
+use lib ("$ENV{GEMC}/api/perl");
+use utils;
+use parameters;
+use geometry;
+use hit;
+use bank;
+use math;
+use materials;
+
+use Math::Trig;
+
+# Help Message
+sub help()
+{
+	print "\n Usage: \n";
+	print "   bdx.pl <configuration filename>\n";
+ 	print "   Will create the BDX using the variation specified in the configuration file\n";
+ 	print "   Note: The passport and .visa files must be present to connect to MYSQL. \n\n";
+	exit;
+}
+
+# Make sure the argument list is correct
+if( scalar @ARGV != 1) 
+{
+	help();
+	exit;
+}
+
+
+# Loading configuration file and paramters
+our %configuration = load_configuration($ARGV[0]);
+
+print "CONFIGURATION IS ::: ",$configuration{"variation"},"\n";
+
+# One can change the "variation" here if one is desired different from the config.dat
+# $configuration{"variation"} = "myvar";
+
+# To get the parameters proper authentication is needed.
+# our %parameters    = get_parameters(%configuration);
+
+# Loading FT specific subroutines
+require "./hit.pl";
+require "./bank.pl";
+#require "./geometry_with_dump.pl";
+require "./geometry_final.pl";
+require "./materials.pl";
+
+define_banks();
+define_bdx_hits();
+define_bdx_materials();
+make_all(); 
+    
+    
