6ccc67630f6d510de9384f54191179b818aa80de
Marco Spreafico <sprea@SpreaBook-Pro.local>
2026-04-08 12:56:17 +0200

    Corrected geometry and hit process. In Final configuration added sector to distinguish between BGO (sector 0 and 1), PANDA (sector 2) and PRad (sector 3)

--- experiments/bdx/bank.pl
+++ experiments/bdx/bank.pl
@@ -72,19 +72,14 @@ sub define_bdx_bank
     $bankname = "crs";
     
     insert_bank_variable(\%configuration, $bankname, "bankid", $bankId, "Di", "$bankname bank ID");
-    insert_bank_variable(\%configuration, $bankname, "sector",       1, "Di", "sector number");
-    insert_bank_variable(\%configuration, $bankname, "xch",          2, "Di", "xch number");
-    insert_bank_variable(\%configuration, $bankname, "ych",          3, "Di", "ych number");
-    insert_bank_variable(\%configuration, $bankname, "zch",          4, "Di", "zch number");
-    insert_bank_variable(\%configuration, $bankname, "adcl",         5, "Di", "adcl");
-    insert_bank_variable(\%configuration, $bankname, "adcr",         6, "Di", "adcr");
-    insert_bank_variable(\%configuration, $bankname, "tdcl",         7, "Di", "tdcl");
-    insert_bank_variable(\%configuration, $bankname, "tdcr",         8, "Di", "tdcr");
-    insert_bank_variable(\%configuration, $bankname, "adcb",         9, "Di", "adcb");
-    insert_bank_variable(\%configuration, $bankname, "adcf",         10, "Di", "adcf");
-    insert_bank_variable(\%configuration, $bankname, "tdcb",        11, "Di", "tdcb");
-    insert_bank_variable(\%configuration, $bankname, "tdcf",        12, "Di", "tdcf");
-    insert_bank_variable(\%configuration, $bankname, "hitn",        99, "Di", "hit number"); 
+    insert_bank_variable(\%configuration, $bankname, "hitn",       1, "Di", "hit number");
+    insert_bank_variable(\%configuration, $bankname, "sector",          2, "Di", "module number");
+    insert_bank_variable(\%configuration, $bankname, "layer",          3, "Di", "plane number");
+    insert_bank_variable(\%configuration, $bankname, "component",          4, "Di", "x + 100*y");
+    insert_bank_variable(\%configuration, $bankname, "ADC_order",         5, "Di", "ADC_order");
+    insert_bank_variable(\%configuration, $bankname, "ADC_order",         6, "Di", "energy measured (keV)");
+    insert_bank_variable(\%configuration, $bankname, "ADC_time",         7, "Di", "time");
+    insert_bank_variable(\%configuration, $bankname, "ADC_ped",         8, "Di", "pedestal");
 
 
 }
